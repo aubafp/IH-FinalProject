366b7fed0cb635d358be76070bf8d61a718d33d5
Auba Fuster <aubafp@gmail.com>
2024-03-23 12:47:25 +0100

    change supabase credentials

--- src/api/supabase.js
+++ src/api/supabase.js
@@ -1,7 +1,7 @@
 const API_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwenJhc2xod2R2ZG9pa2FrdWZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA4NzkwMzMsImV4cCI6MjAyNjQ1NTAzM30.nqfuLRg_URrz3x0fMBw3CPj1YV9QCdedHkNief23aU4'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrZXJlemR0Z3F6bGh5amlhZXRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTExODQwMzIsImV4cCI6MjAyNjc2MDAzMn0.v19PnoEna5Mv6-Bjs88EzgDcmEWq99Y_sGHGFbbcgLQ'
 
-const PROJECT_URL = 'https://wpzraslhwdvdoikakufh.supabase.co'
+const PROJECT_URL = 'https://kkerezdtgqzlhyjiaetq.supabase.co'
 
 import { createClient } from '@supabase/supabase-js'
 
